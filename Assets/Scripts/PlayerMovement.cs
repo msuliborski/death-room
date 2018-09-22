8942b7036f4cc72538469987f1e5cdf6d7a4ca8f
Michał Suliborski <msuliborski@gmail.com>
2018-09-22 08:19:59 +0200

    tiles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody2D _rigidbody;
     public Animator animator;
-	float maxForce = 900f;
+	float vel = 4f;
 
 	void Start () {
 		_rigidbody = gameObject.GetComponent<Rigidbody2D>();
@@ -16,48 +16,47 @@ public class PlayerMovement : MonoBehaviour {
 	
 	void LateUpdate () {
 
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            animator.SetBool("Speed_up", true);
-            _rigidbody.AddForce(new Vector2(0, maxForce));
+        if (Input.GetKeyDown(KeyCode.W)){
+			stopAll();
+            animator.SetBool("Speed_up", true); 
         }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            animator.SetBool("Speed_up", false);
-            _rigidbody.AddForce(new Vector2(0, -maxForce));
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
+        else if (Input.GetKeyDown(KeyCode.S)){
+			stopAll();
             animator.SetBool("Speed_down", true);
-            _rigidbody.AddForce(new Vector2(0, -maxForce));
         }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            animator.SetBool("Speed_down", false);
-            _rigidbody.AddForce(new Vector2(0, maxForce));
-        }
-
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        else if (Input.GetKeyDown(KeyCode.A)){
+			stopAll();
             animator.SetBool("Speed_left", true);
-            _rigidbody.AddForce(new Vector2(-maxForce, 0));
         }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            animator.SetBool("Speed_left", false);
-            _rigidbody.AddForce(new Vector2(maxForce, 0));
+        else if (Input.GetKeyDown(KeyCode.D)){
+			stopAll();
+            animator.SetBool("Speed_right", true);
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            animator.SetBool("Speed_right", true);
-            _rigidbody.AddForce(new Vector2(maxForce, 0));
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
+		
+		if (Input.GetKeyUp(KeyCode.W)) animator.SetBool("Speed_up", false); 
+		if (Input.GetKeyUp(KeyCode.S)) animator.SetBool("Speed_down", false); 
+		if (Input.GetKeyUp(KeyCode.A)) animator.SetBool("Speed_left", false); 
+		if (Input.GetKeyUp(KeyCode.D)) animator.SetBool("Speed_right", false); 
+
+
+
+        if (Input.GetKey(KeyCode.W)) _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, vel, 0);
+        if (Input.GetKeyUp(KeyCode.W)) _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0, 0);
+        if (Input.GetKey(KeyCode.S)) _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, -vel, 0);
+        if (Input.GetKeyUp(KeyCode.S)) _rigidbody.velocity = new Vector3(_rigidbody.velocity.x, 0, 0);
+        if (Input.GetKey(KeyCode.A)) _rigidbody.velocity = new Vector3(-vel, _rigidbody.velocity.y, 0);
+        if (Input.GetKeyUp(KeyCode.A)) _rigidbody.velocity = new Vector3(0, _rigidbody.velocity.y, 0);
+        if (Input.GetKey(KeyCode.D)) _rigidbody.velocity = new Vector3(vel, _rigidbody.velocity.y, 0);
+        if (Input.GetKeyUp(KeyCode.D)) _rigidbody.velocity = new Vector3(0, _rigidbody.velocity.y, 0);
+	}
+	void stopAll(){
+            animator.SetBool("Speed_up", false);
+    
+            animator.SetBool("Speed_down", false);
+    
+            animator.SetBool("Speed_left", false);
+    
             animator.SetBool("Speed_right", false);
-            _rigidbody.AddForce(new Vector2(-maxForce, 0));
-        }
 	}
 }
