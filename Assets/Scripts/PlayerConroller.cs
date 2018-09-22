4b1ae87adf860ad4785c1e2f12f43eb9232b1f54
jedrekszor <jedrekszor@wp.pl>
2018-09-22 17:00:14 +0200

    all deaths

--- Assets/Scripts/PlayerConroller.cs
+++ Assets/Scripts/PlayerConroller.cs
@@ -3,44 +3,47 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerConroller : MonoBehaviour {
-    // Use this for initialization
+    
     private DeathManager dm;
     public GameObject player;
     public GameObject blood;
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        
-    }
+    public GameObject leg1;
+    public GameObject leg2;
+    public GameObject laserCut;
+    public GameObject _shot;
 
-    // Update is called once per frame
-    void Update () {
-        if (Input.GetKeyDown(KeyCode.P)) Death();
-	}
-
-	public void Death()
-	{
-        Instantiate(blood, transform.position, Quaternion.identity);
-        player.SetActive(false);
-	}
-
+    
     public void Kaboom()
     {
-
+        Instantiate(leg1, transform.position, Quaternion.identity);
+        Instantiate(leg2, new Vector3(transform.position.x-2, transform.position.y, transform.position.z), Quaternion.identity);
+        Instantiate(blood, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     public void Lazers()
     {
-
+        Instantiate(laserCut, transform.position, Quaternion.identity);
+        Instantiate(blood, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     public void Smash()
     {
-
+        Instantiate(blood, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
     public void Fall()
     {
+        Destroy(gameObject);
+    }
 
+    public void shot()
+    {
+        Instantiate(_shot, transform.position, Quaternion.identity);
+        Instantiate(blood, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 }
 
