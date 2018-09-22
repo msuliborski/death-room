ec1f63eb229ed12615d0d8201d3e6fef0c3f4b26
jedrekszor <jedrekszor@wp.pl>
2018-09-22 05:31:24 +0200

    animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody2D _rigidbody;
-
+    public Animator animator;
 	float maxForce = 900f;
 
 	void Start () {
@@ -16,16 +16,48 @@ public class PlayerMovement : MonoBehaviour {
 	
 	void LateUpdate () {
 
-		if (Input.GetKeyDown(KeyCode.W)) _rigidbody.AddForce(new Vector2(0, maxForce));
-		if (Input.GetKeyUp(KeyCode.W)) _rigidbody.AddForce(new Vector2(0, -maxForce));
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetBool("Speed_up", true);
+            _rigidbody.AddForce(new Vector2(0, maxForce));
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("Speed_up", false);
+            _rigidbody.AddForce(new Vector2(0, -maxForce));
+        }
 
-		if (Input.GetKeyDown(KeyCode.S)) _rigidbody.AddForce(new Vector2(0, -maxForce));
-		if (Input.GetKeyUp(KeyCode.S)) _rigidbody.AddForce(new Vector2(0, maxForce));
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            animator.SetBool("Speed_down", true);
+            _rigidbody.AddForce(new Vector2(0, -maxForce));
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("Speed_down", false);
+            _rigidbody.AddForce(new Vector2(0, maxForce));
+        }
 
-		if (Input.GetKeyDown(KeyCode.A)) _rigidbody.AddForce(new Vector2(-maxForce, 0));
-		if (Input.GetKeyUp(KeyCode.A)) _rigidbody.AddForce(new Vector2(maxForce, 0));
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetBool("Speed_left", true);
+            _rigidbody.AddForce(new Vector2(-maxForce, 0));
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            animator.SetBool("Speed_left", false);
+            _rigidbody.AddForce(new Vector2(maxForce, 0));
+        }
 
-		if (Input.GetKeyDown(KeyCode.D)) _rigidbody.AddForce(new Vector2(maxForce, 0));
-		if (Input.GetKeyUp(KeyCode.D)) _rigidbody.AddForce(new Vector2(-maxForce, 0));
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetBool("Speed_right", true);
+            _rigidbody.AddForce(new Vector2(maxForce, 0));
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            animator.SetBool("Speed_right", false);
+            _rigidbody.AddForce(new Vector2(-maxForce, 0));
+        }
 	}
 }
