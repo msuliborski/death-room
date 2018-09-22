47a0f595f728ed26e96e4b28702e0072c40ac290
jedrekszor <jedrekszor@wp.pl>
2018-09-22 06:32:59 +0200

    turret

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+
+    public GameObject cannon;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Gun());
+            
+	}
+	IEnumerator Gun()
+    {
+        yield return new WaitForSeconds(1f);
+        cannon.SetActive(true);
+    }
+}
