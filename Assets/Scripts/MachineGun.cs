1d5b84ef8ae5961e31a9953a3e16f27aedf90629
unknown <antonio25698@gmail.com>
2018-09-22 03:52:46 +0200

    machineGun

--- Assets/Scripts/MachineGun.cs
+++ Assets/Scripts/MachineGun.cs
@@ -4,41 +4,41 @@ using UnityEngine;
 
 public class MachineGun : MonoBehaviour
 {
-    public GameObject bullet;
+    public GameObject Bullet;
+    public GameObject SpawnPoint;
+    public GameObject Player;
+
     public float firerate = 0.5f;
     public float velocity = 15f;
-    public float rotationOffset = 5f;
+    public float rotationOffset = 0.2f;
+    public float ammunition = 10;
+
     bool flag = true;
-    public GameObject inst;
-
-    // Use this for initialization
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
-        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position; //zmienić na pozycję postaci
+        Vector2 direction = Player.transform.position - transform.position; 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationOffset * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime);
 
-        if (flag)
+        if (flag && ammunition >= 0)
         {
             direction.Normalize();
-            GameObject shot = (GameObject)Instantiate(bullet, inst.transform.position, Quaternion.identity);
-            shot.GetComponent<Rigidbody2D>().velocity = direction * velocity;
+            GameObject shot = (GameObject)Instantiate(Bullet, SpawnPoint.transform.position, Quaternion.identity);
+            shot.GetComponent<Rigidbody2D>().velocity = transform.right * velocity;
             StartCoroutine(Cooldown());
-            Destroy(shot, 1);
+            Destroy(shot, 5);
+            ammunition--;
         }
     }
 
     IEnumerator Cooldown()
     {
         flag = false;
+
         yield return new WaitForSeconds(firerate);
         flag = true;
     }
