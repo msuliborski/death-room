4cd88f94aa61ca6b2fe1208c0033acd2bbfdf678
Michał Suliborski <msuliborski@gmail.com>
2018-09-23 06:55:50 +0200

    waed

---
+++ Assets/startScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startScript : MonoBehaviour {
+	GameObject _player;
+	// Use this for initialization
+	void Start () {
+		_player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)){
+			_player.transform.position = new Vector2(1.63f, 2.2f);
+			gameObject.SetActive(false);
+		}
+		
+	}
+}
