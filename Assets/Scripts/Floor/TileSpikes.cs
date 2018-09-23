e0002ef06fa5bd88f1a4149fae1c5dc2e2468e21
jedrekszor <jedrekszor@wp.pl>
2018-09-23 05:41:45 +0200

    booby trap

--- Assets/Scripts/Floor/TileSpikes.cs
+++ Assets/Scripts/Floor/TileSpikes.cs
@@ -6,9 +6,15 @@ public class TileSpikes : MonoBehaviour {
 	
 	bool isTriggered = false;
  	SpriteRenderer spriteRenderer;
+    public GameObject boom;
+    public GameObject collider;
+    public AudioClip peep;
+    AudioSource source;
 
     void Start(){
         spriteRenderer = GetComponent<SpriteRenderer>();
+        source = GetComponent<AudioSource>();
+        source.clip = peep;
     }
 
 	void Update () {
@@ -18,8 +24,19 @@ public class TileSpikes : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D _colliderPlayer){
 		if(!isTriggered){
 			isTriggered = true;
-        	//spriteRenderer.color = new Color(0.1f, 0.4f, 0.255f, 1f);
+            //spriteRenderer.color = new Color(0.1f, 0.4f, 0.255f, 1f);
+            StartCoroutine(boobyTrap());
         	spriteRenderer.color = Color.grey;
 	 	}
 	}
+
+    IEnumerator boobyTrap()
+    {
+        source.PlayOneShot(source.clip);
+        yield return new WaitForSeconds(0.5f);
+        collider.SetActive(true);
+        Instantiate(boom, transform.position, Quaternion.identity);
+
+
+    }
 }
